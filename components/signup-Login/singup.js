ee27608283df5699ea68fd78534a9e6a07f48e81
Mohamed Yusuf Mohamed <107251702+Mohamed-Y-Mohamed@users.noreply.github.com>
2024-02-27 21:17:40 +0000

    Signup function

--- components/signup-Login/singup.js
+++ components/signup-Login/singup.js
@@ -1,61 +1,40 @@
- //imports section
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
- import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
- //firebase link
- // https://firebase.google.com/docs/web/setup#available-libraries
+import { app } from "../firebase-setup";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, collection, doc, setDoc } from 'firebase/firestore/lite';
 
- //  web app's Firebase configuration
- const firebaseConfig = {
-   apiKey: "AIzaSyD_3bhaBq78XxKkLU4fZkZMt04DVYR--vg",
-   authDomain: "artvisio-b3b76.firebaseapp.com",
-   projectId: "artvisio-b3b76",
-   storageBucket: "artvisio-b3b76.appspot.com",
-   messagingSenderId: "705790621970",
-   appId: "1:705790621970:web:11c06cfa679c1e251e1bcb"
- };
+const auth = getAuth();
+const db = getFirestore(app);
 
- // Initialize Firebase
- const app = initializeApp(firebaseConfig);
- 
- const auth = getAuth();
+const submit = document.getElementById("signup-submit");
 
+submit.addEventListener('click', function(event) {
+    event.preventDefault();
 
- const submit = document.getElementById("signup-submit");
+    const email = document.getElementById("Signup-Email").value;
+    const password = document.getElementById("Signup-Pass").value;
+    const firstname = document.getElementById("Signup-FN").value;
+    const lastname = document.getElementById("Signup-LN").value;
 
-//sign up with submit button click
- submit.addEventListener('click',function(event) {
-//remove defualt reload
- event.preventDefault();
+    let userRole = document.getElementById('signup-role').checked ? 'artist' : 'art enthusiast';
 
- // store signup data
- const email = document.getElementById("Signup-Email").value;
+    createUserWithEmailAndPassword(auth,email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            alert("Creating account. User ID: " + user.uid);
 
- const password = document.getElementById("Signup-Pass").value;
-
- const firstname = document.getElementById("Signup-FN").value;
-
- const lastname = document.getElementById("Signup-LN").value;
-
-//role check for switcher
-let role = "";
-if (document.getElementById('signup-role').checked) {
-    role = 'artist';
-} else {
-    role = 'art enthusiast';
-}
-
-
- //firebase signup
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    const user = userCredential.user;
-    alert(" creatinh account." + " User ID:"+ user.uid);
-
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    alert("unablhgjghjybve to create account.\n"+errorCode+"\n"+errorMessage )
-  });
-} )
-
+            return setDoc(doc(db, "users", user.uid), {
+                firstName: firstname,
+                lastName: lastname,
+                email: email,
+                role: userRole
+            });
+        })
+        .then(() => {
+            alert("User data added successfully!");
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert("Unable to create account.\n" + errorCode + "\n" + errorMessage);
+        });
+});
