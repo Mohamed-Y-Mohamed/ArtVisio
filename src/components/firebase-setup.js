5362d5964370f5a8ade8a200ceed17e161eab406
Mohamed Yusuf Mohamed <107251702+Mohamed-Y-Mohamed@users.noreply.github.com>
2024-04-07 00:33:16 +0100

    secure api key

--- src/components/firebase-setup.js
+++ src/components/firebase-setup.js
@@ -6,12 +6,12 @@ import {
 // Define the configuration for the Firebase application.
 // This configuration includes the API key, Auth domain, Project ID, Storage Bucket, Messaging Sender ID, and App ID.
 const firebaseConfig = {
-    apiKey: "AIzaSyC8CnGwlFSZ1q-BTj9SaeHaY3K41f8n1dM",
-    authDomain: "artvisio-fae52.firebaseapp.com",
-    projectId: "artvisio-fae52",
-    storageBucket: "artvisio-fae52.appspot.com",
-    messagingSenderId: "96525313731",
-    appId: "1:96525313731:web:c28400467e581836646476"
+    apiKey:import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
 // Initialize the Firebase application with the specified configuration.
